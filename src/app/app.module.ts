ca1db50123be5d62d39ca67626f4a221644837bf
ibdkn <ibudiakina@gmail.com>
2024-07-04 00:12:46 +0300

    added slider for main page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,16 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { LayoutComponent } from './shared/layout/layout.component';
-import { HeaderComponent } from './shared/layout/header/header.component';
-import { FooterComponent } from './shared/layout/footer/footer.component';
-import { MainComponent } from './views/main/main.component';
-import { BurgerMenuComponent } from './shared/components/burger-menu/burger-menu.component';
-import { SideNavComponent } from './shared/components/side-nav/side-nav.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {LayoutComponent} from './shared/layout/layout.component';
+import {HeaderComponent} from './shared/layout/header/header.component';
+import {FooterComponent} from './shared/layout/footer/footer.component';
+import {MainComponent} from './views/main/main.component';
+import {BurgerMenuComponent} from './shared/components/burger-menu/burger-menu.component';
+import {SideNavComponent} from './shared/components/side-nav/side-nav.component';
+import {CarouselModule} from "ngx-owl-carousel-o";
+import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
 
 @NgModule({
   declarations: [
@@ -22,9 +24,12 @@ import { SideNavComponent } from './shared/components/side-nav/side-nav.componen
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    CarouselModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
--- src/app/views/main/main.component.ts
+++ src/app/views/main/main.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {OwlOptions} from "ngx-owl-carousel-o";
 
 @Component({
   selector: 'app-main',
@@ -7,9 +8,36 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MainComponent implements OnInit {
 
+  customOptionsMain: OwlOptions = {
+    loop: true,
+    mouseDrag: false,
+    touchDrag: false,
+    pullDrag: false,
+    margin: 10,
+    dots: true,
+    dotsSpeed: 1000,
+    navSpeed: 1000,
+    autoplay: true,
+    autoplaySpeed: 1000,
+    navText: ['', ''],
+    responsive: {
+      0: {
+        items: 1
+      },
+      400: {
+        items: 1
+      },
+      740: {
+        items: 1
+      },
+    },
+    nav: false
+  }
+
   constructor() { }
 
   ngOnInit(): void {
+
   }
 
 }
